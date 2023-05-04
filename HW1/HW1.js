44050f3b4043f6249ce043cad63d928e6a438d7a
BigGrayOwl <semehenpavlo@gmail.com>
2023-05-04 09:47:44 +0300

    HW1  - HW1.html  - HW1.js

---
+++ HW1/HW1.js
@@ -0,0 +1,43 @@
+let s1 = 'hello'
+console.log(s1)
+let s2 = 'owu'
+console.log(s2)
+let s3 = 'com'
+console.log(s3)
+let s4 = 'ua'
+console.log(s4)
+let s5 = 1
+console.log(s5)
+let s6 = 10
+console.log(s6)
+let s7 = -999
+console.log(s7)
+let s8 = 123
+console.log(s8)
+let s9 = 3.14
+console.log(s9)
+let s10 = 2.7
+console.log(s10)
+let s11 = 16
+console.log(s11)
+let s12 = true
+console.log(s12)
+let s13 = false
+console.log(s13)
+let firstName = 'Pavlo'
+let middleName = 'Petrovych'
+let lastName = 'Semehen'
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+let a = 100
+console.log(typeof a)
+let b ='100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+let name = prompt('Enter your name')
+console.log(name)
+let middlename = prompt('Enter your middle name')
+console.log(middlename)
+let age = prompt('Enter your age')
+console.log(age)
